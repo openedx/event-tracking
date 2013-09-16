53bd776b0c620f95ea89c70a68ab02ce215d93ee
Gabe Mulley <gabe@edx.org>
2013-09-16 16:54:02 -0400

    adding a bare django app

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='event-tracking',
+    version='0.1',
+    packages=['eventtracking'],
+    include_package_data=True,
+    license='GPLv3 License',
+    description='A simple event tracking app for Django.',
+    long_description=README,
+    url='http://code.edx.org/',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
