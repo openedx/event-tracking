48462e64aae55d3b388219a1ec25e3af94377869
muhammad-ammar <mammar@gmail.com>
2015-11-04 17:14:28 +0500

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='event-tracking',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     license='AGPLv3 License',
