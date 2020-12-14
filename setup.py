8935aa84bafa25c97da151b814563653b25290f6
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-12-14 16:21:18 +0500

    Fixing pypi upload.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ REQUIREMENTS = load_requirements('requirements/base.in')
 
 setup(
     name='event-tracking',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     license='AGPLv3 License',
